e7d8371ed9b43fefa7cc1bb53bf81438bfc8f8f0
wulabing <hanbing151400@gmail.com>
2020-02-06 15:43:04 +0800

    1.0.7 Nginx配置文件 XFF添加 感谢 tg:@Cliwired

--- install.sh
+++ install.sh
@@ -465,6 +465,8 @@ nginx_conf_add(){
         proxy_redirect off;
         proxy_pass http://127.0.0.1:10000;
         proxy_http_version 1.1;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
         proxy_set_header Host \$http_host;
