9a17d88fd2a8c33916b8e2e520430c6d06a84fe0
wulabing <hanbing151400@gmail.com>
2020-02-24 10:24:55 +0800

    1.1.3.2

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ OK="${Green}[OK]${Font}"
 Error="${Red}[错误]${Font}"
 
 # 版本
-shell_version="1.1.3.1"
+shell_version="1.1.3.2"
 shell_mode="None"
 github_branch="master"
 version_cmp="/tmp/version_cmp.tmp"
