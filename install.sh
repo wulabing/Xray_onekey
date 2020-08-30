46d08390d5f0feaf0d24ef93cfeea2e41727ad26
wulabing <hanbing151400@gmail.com>
2020-08-30 21:57:52 +0800

    [modify] 修复版本识别异常问题 版本号变更

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ Error="${Red}[错误]${Font}"
 Warning="${Red}[警告]${Font}"
 
 # 版本
-shell_version="1.1.6.14"
+shell_version="1.1.6.15"
 shell_mode="None"
 github_branch="dev"
 version_cmp="/tmp/version_cmp.tmp"
