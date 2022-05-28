3d8201f987e7ba03167fa27d85038be4f9f27cc8
misakano7545 <misakano7545@protonmail.com>
2022-05-28 17:58:31 +0800

    Acme.sh IPv6 support

--- install.sh
+++ install.sh
@@ -449,6 +449,17 @@ function acme() {
         print_ok "已启动 wgcf-warp"
       fi
     fi
+  elif "$HOME"/.acme.sh/acme.sh --issue -d "${domain}" -k ec-256 --webroot "$website_dir" --force --listen-v6; then
+    print_ok "SSL 证书生成成功"
+    sleep 2
+    if "$HOME"/.acme.sh/acme.sh --installcert -d "${domain}" --fullchainpath /ssl/xray.crt --keypath /ssl/xray.key --reloadcmd "systemctl restart xray" --reloadcmd "systemctl restart nginx" --ecc --force; then
+      print_ok "SSL 证书配置成功"
+      sleep 2
+      if [[ -n $(type -P wgcf) && -n $(type -P wg-quick) ]]; then
+        wg-quick up wgcf >/dev/null 2>&1
+        print_ok "已启动 wgcf-warp"
+      fi
+    fi
   else
     print_error "SSL 证书生成失败"
     rm -rf "$HOME/.acme.sh/${domain}_ecc"
