31eaf885b75691a8d59ad30fa1b2d4c968642e47
wulabing <wulabing@gmail.com>
2022-06-08 20:22:04 +0800

    Xray-0.2.1
    [fix] IPV6 无法连接问题

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ OK="${Green}[OK]${Font}"
 ERROR="${Red}[ERROR]${Font}"
 
 # 变量
-shell_version="0.2.0"
+shell_version="0.2.1"
 github_branch="nginx_forward"
 xray_conf_dir="/usr/local/etc/xray"
 website_dir="/www/xray_web/"
